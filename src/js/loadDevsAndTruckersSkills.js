b50c0e927b154d7d81d76cc7cf58956686ee1101
iblind <iblinderman@iblinderman-macbookpro.roam.corp.google.com>
2018-04-22 14:19:52 -0400

    d3 centric implementation of bars ie no for loops

--- src/js/loadDevsAndTruckersSkills.js
+++ src/js/loadDevsAndTruckersSkills.js
@@ -29,35 +29,42 @@ export default function loadDevsAndTruckersSkills(){
 
     const devAndTruckerSkills = response[0];
 
-    const choreographers = response[1]
-    const dentists = response[2]
-    const nurses = response[3]
-    const chiropractors = response[4]
-    const farmers = response[5]
-    const construction_managers = response[6]
-    const firefighters = response[7]
-    const geographers = response[8]
-    const embalmers = response[9]
-    const pipelayers = response[10]
-    const allProfessionSkills = response[11];
+    let allJobSkillsRaw=[]
 
-    const allJobSkills = [choreographers,dentists,nurses,chiropractors,farmers,construction_managers,
-                          firefighters,geographers,embalmers,pipelayers]
+// NB i=1 here to exclude first entry in data loaded, which reprensents devs comparison
+    for (i=1;i<response.length;i++){
+      allJobSkillsRaw.push(response[i])
+    }
+
+    // const choreographers = response[1]
+    // const dentists = response[2]
+    // const nurses = response[3]
+    // const chiropractors = response[4]
+    // const farmers = response[5]
+    // const construction_managers = response[6]
+    // const firefighters = response[7]
+    // const geographers = response[8]
+    // const embalmers = response[9]
+    // const pipelayers = response[10]
+    // const allProfessionSkills = response[11];
+
+    // const allJobSkills = [choreographers,dentists,nurses,chiropractors,farmers,construction_managers,
+    //                       firefighters,geographers,embalmers,pipelayers]
     const allJobSkillsNames=['choreographers','dentists','nurses','chiropractors','farmers','construction_managers',
                           'firefighters','geographers','embalmers','pipelayers']
 
-    const allJobSkillsNamesObjects=[
-      [{'name':'choreographers'}],
-      [{'name':'dentists'}],
-      [{'name':'nurses'}],
-      [{'name':'chiropractors'}],
-      [{'name':'farmers'}],
-      [{'name':'construction_managers'}],
-      [{'name':'firefighters'}],
-      [{'name':'geographers'}],
-      [{'name':'embalmers'}],
-      [{'name':'pipelayers'}]
-    ]
+    // const allJobSkillsNamesObjects=[
+    //   [{'name':'choreographers'}],
+    //   [{'name':'dentists'}],
+    //   [{'name':'nurses'}],
+    //   [{'name':'chiropractors'}],
+    //   [{'name':'farmers'}],
+    //   [{'name':'construction_managers'}],
+    //   [{'name':'firefighters'}],
+    //   [{'name':'geographers'}],
+    //   [{'name':'embalmers'}],
+    //   [{'name':'pipelayers'}]
+    // ]
 
     devAndTruckerSkills.forEach(skill=>{
       skill.difference = Math.abs(+skill.devs- +skill.truckers)
@@ -72,7 +79,7 @@ export default function loadDevsAndTruckersSkills(){
       xSum+=devAndTruckerSkills[i]['difference'] + stackPadding
     }
 
-    allJobSkills.forEach(jobAndTruckerSkills=>{
+    allJobSkillsRaw.forEach(jobAndTruckerSkills=>{
       let i;
       let xSum=0;
       let stackPadding=1;
@@ -81,6 +88,12 @@ export default function loadDevsAndTruckersSkills(){
         xSum+= +jobAndTruckerSkills[i]['difference'] + stackPadding
       }
     })
+
+    let allJobSkillsFlat = [].concat.apply([], allJobSkillsRaw);
+
+    let allJobSkillsGrouped = d3.nest()
+      .key(d=>d.job_compared_name)
+      .entries(allJobSkillsFlat)
 
     // console.log(allJobSkills);
 
@@ -99,14 +112,28 @@ export default function loadDevsAndTruckersSkills(){
 // original
     const skillSections = chartSvg.selectAll('g.skill-section').data(devAndTruckerSkills).enter().append('g.skill-section')
 
-// Creating group elements tied to data from multiple jobs
-    let jobSkillGroupObject={}
-    for(i=0; i<allJobSkillsNames.length; i++){
-      jobSkillGroupObject[`${allJobSkillsNames[i]}`]=chartSvg.selectAll(`g.skill-section-${allJobSkillsNames[i]}`)
-      .data(allJobSkills[i])
+    const skillSectionsAllJobs = chartSvg.selectAll('g.all-skills')
+      .data(allJobSkillsGrouped)
       .enter()
-      .append(`g.skill-section-${allJobSkillsNames[i]}`)
-    }
+      .append('g')
+      .at('class',d=> 'all-skills-'+d.key)
+
+    const skillItemsAllJobs = skillSectionsAllJobs.selectAll('g.skill-item')
+      .data(d=>d.values)
+      .enter()
+      .append('g')
+      .at('class',d=>'skill-item-'+d.skills.replace(/ /g,'_'))
+
+
+
+// Creating group elements tied to data from multiple jobs
+    // let jobSkillGroupObject={}
+    // for(i=0; i<allJobSkillsNames.length; i++){
+    //   jobSkillGroupObject[`${allJobSkillsNames[i]}`]=chartSvg.selectAll(`g.skill-section-${allJobSkillsNames[i]}`)
+    //   .data(allJobSkills[i])
+    //   .enter()
+    //   .append(`g.skill-section-${allJobSkillsNames[i]}`)
+    // }
 
 
     const YBUMP = 20;
@@ -126,23 +153,30 @@ export default function loadDevsAndTruckersSkills(){
       .at('r',5)
       .st('fill','#EB5757')
 
+// Adding original text labels
     const skillSectionsText = skillSections
-    .at('transform', (d,i)=>{
-      return 'translate(50,'+ (YBUMP+(i*YINTERVAL)) +')'
-    })
-    .append("text")
-    .text(d=>d.skills)
-    .st('text-anchor','right')
+      .at('transform', (d,i)=>{
+        return 'translate(50,'+ (YBUMP+(i*YINTERVAL)) +')'
+      })
+      .append("text")
+      .text(d=>d.skills)
+      .st('text-anchor','right')
+
+    const skillSectionsTextAllJobs = skillSectionsAllJobs
+      .append("text")
+      .at('transform','translate(250,0)')
+      .text(d=>d.key)
+      .st('text-anchor','right')
+
 
 // Adding other text labels
-    let allJobLabels = {}
-    for(i=0; i<allJobSkillsNames.length; i++){
-      allJobLabels[`${allJobSkillsNames[i]}`]=jobSkillGroupObject[allJobSkillsNames[i]].select(`text.all-jobs.job-${allJobSkillsNames[i]}`)
-      .data(allJobSkillsNamesObjects[i])
-      .enter()
-      .append(`text.all-jobs.job-${allJobSkillsNames[i]}`)
-
-    }
+    // let allJobLabels = {}
+    // for(i=0; i<allJobSkillsNames.length; i++){
+    //   allJobLabels[`${allJobSkillsNames[i]}`]=jobSkillGroupObject[allJobSkillsNames[i]].select(`text.all-jobs.job-${allJobSkillsNames[i]}`)
+    //   .data(allJobSkillsNamesObjects[i])
+    //   .enter()
+    //   .append(`text.all-jobs.job-${allJobSkillsNames[i]}`)
+    // }
 
 
     const axisLines = skillSections.append('line.skill-axis')
@@ -156,13 +190,16 @@ export default function loadDevsAndTruckersSkills(){
 // Original bars signifying different skills
     const axisDifferenceRects = skillSections.append('rect.skill-difference-axis')
 
+    const axisDifferenceRectsAllJobs = skillItemsAllJobs.append('rect.skill-difference-axis-all-jobs')
+
 // Looping through array of data to create, for each job, bars signifying different skills
-    let jobSkillGroupDifferenceRects={}
-    for(i=0; i<allJobSkillsNames.length; i++){
-      jobSkillGroupDifferenceRects[`${allJobSkillsNames[i]}`]=jobSkillGroupObject[allJobSkillsNames[i]].append(`rect.other-job-skills.skill-difference-axis-${allJobSkillsNames[i]}`)
-    }
+    // let jobSkillGroupDifferenceRects={}
+    // for(i=0; i<allJobSkillsNames.length; i++){
+    //   jobSkillGroupDifferenceRects[`${allJobSkillsNames[i]}`]=jobSkillGroupObject[allJobSkillsNames[i]].append(`rect.other-job-skills.skill-difference-axis-${allJobSkillsNames[i]}`)
+    // }
 
 
+// Adding difference rectangles for devs and truckers
     axisDifferenceRects
     .at('x', d=>XBUMP+xScale(d.truckers))
     .at('width',d=> xScale(d.difference))
@@ -170,15 +207,16 @@ export default function loadDevsAndTruckersSkills(){
     .st('fill','#E530BE')
     .st('opacity',0)
 
+// Adding difference retangles for all other jobs
 
-    for(i=0; i<allJobSkillsNames.length; i++){
-      jobSkillGroupDifferenceRects[allJobSkillsNames[i]]
-      .at('x', d=>XBUMP+xScale(d.job_selected))
-      .at('width',d=> xScale(d.difference))
-      .at('height',3)
-      .st('fill','#E530BE')
-      .st('opacity',0)
-    }
+    // for(i=0; i<allJobSkillsNames.length; i++){
+    //   jobSkillGroupDifferenceRects[allJobSkillsNames[i]]
+    //   .at('x', d=>XBUMP+xScale(d.job_selected))
+    //   .at('width',d=> xScale(d.difference))
+    //   .at('height',3)
+    //   .st('fill','#E530BE')
+    //   .st('opacity',0)
+    // }
 
 
     BUTTON_Skill_Difference.on('click',()=>{
@@ -222,33 +260,44 @@ BUTTON_Stack_SkillDifference.on('click',()=>{
   })
 
 BUTTON_Stack_AllJobs_Skills.on('click',()=>{
-  d3.selectAll('rect.other-job-skills')
-  .st('opacity',1)
 
-  let yDistanceFromTop = 0
+  axisDifferenceRectsAllJobs
+  .at('x',d=> xScaleEucledian(d.xCoordStacked))
+  .at('width',d=> xScaleEucledian(d.difference))
+  .at('height',3)
+  .st('fill','#E530BE')
+  .at('transform',(d,i)=>{
+    return 'translate(250,0)'
+  })
+  .on('mouseenter',d=>console.log(d.skills))
 
-  for(i=0; i<allJobSkillsNames.length; i++){
+  skillSectionsAllJobs.at('transform',(d,i)=>{
+    return 'translate(0,'+(i*20+20)+')'
+  })
 
-    yDistanceFromTop=i*20+20;
-
-    // console.log(jobSkillGroupDifferenceRects[allJobSkillsNames[3]]);
-
-    jobSkillGroupDifferenceRects[allJobSkillsNames[i]]
-      .at('x', d=>XBUMP+xScaleEucledian(d.xCoordStacked))
-      .at('width',d=> xScaleEucledian(d.difference))
-      .at('height',3)
-      .transition()
-      .at('transform',`translate(0,${yDistanceFromTop})`)
-      .st('fill','#E530BE')
-      .st('opacity',1)
-
-    //Adding labels
-     allJobLabels[allJobSkillsNames[i]]
-      .at('transform',`translate(50,${yDistanceFromTop})`)
-      .st('opacity',1)
-      .text(d => d.name)
-  }
-
+//   d3.selectAll('rect.other-job-skills').st('opacity',1)
+//
+//   let yDistanceFromTop = 0
+//
+//   for(i=0; i<allJobSkillsNames.length; i++){
+//
+//     yDistanceFromTop=i*20+20;
+//
+//     jobSkillGroupDifferenceRects[allJobSkillsNames[i]]
+//       .at('x', d=>XBUMP+xScaleEucledian(d.xCoordStacked))
+//       .at('width',d=> xScaleEucledian(d.difference))
+//       .at('height',3)
+//       .transition()
+//       .at('transform',`translate(0,${yDistanceFromTop})`)
+//       .st('fill','#E530BE')
+//       .st('opacity',1)
+//
+//     //Adding labels
+//      allJobLabels[allJobSkillsNames[i]]
+//       .at('transform',`translate(50,${yDistanceFromTop})`)
+//       .st('opacity',1)
+//       .text(d => d.name)
+//   }
 })
 
 
